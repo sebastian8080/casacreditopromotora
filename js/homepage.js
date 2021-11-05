c6c4dda80324d704c60064d452bfe1ac51e716c3
sebastian8080 <sebas25211@hotmail.com>
2021-11-05 11:39:55 -0500

    Cambios en la pagina home y nuevos archivos js y css

---
+++ js/homepage.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function (e) {
+    var nav = document.getElementById('menu');
+    if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
+            nav.classList.add('navbar-light');
+            nav.classList.add('bg-light');
+            nav.classList.remove('navbar-dark');
+        } else {
+            nav.classList.add('navbar-dark');
+            nav.classList.remove('navbar-light');
+            nav.classList.remove('bg-light');
+        }
+  });
+  
